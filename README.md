e28a32e339bf005903c74fc1cc017184b54c9038
codesharedot <codeto@sent.com>
2019-10-14 11:26:15 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# huobi-token price gnome ticket 
+# iota price gnome ticket 
 
-Gnome 3 desktop ticker for huobi-token price with Python and argos. Rename to huobi-token.1r.60s.py and put in ~/.config/argos
+Gnome 3 desktop ticker for iota price with Python and argos. Rename to iota.1r.60s.py and put in ~/.config/argos
 
 https://pythonbasics.org
--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# rename to huobi-token.1r.60s.py and put in ~.config/argos
+# rename to iota.1r.60s.py and put in ~.config/argos
 import re
 from gi.repository import Gio
 from datetime import datetime
@@ -9,7 +9,7 @@ import json
 import requests
 
 def coin():
-    data = requests.get("https://api.coinmarketcap.com/v1/ticker/huobi-token/")
+    data = requests.get("https://api.coinmarketcap.com/v1/ticker/iota/")
     price = data.json()[0]["price_usd"]
     coin_price = float(("{0:.2f}").format(float(price))) 
     return coin_price
@@ -18,7 +18,7 @@ usd = float(coin())
 lastupdate = datetime.now().strftime("%H:%M:%S")
 
 #print(" $" + str(usd) + " (" + str(lastupdate) + ") | iconName=invest-applet")
-print("huobi-token $" + str(usd) + " | iconName=invest-applet")
+print("iota $" + str(usd) + " | iconName=invest-applet")
 print("---")
 #print("Kraken: $" + str(usd) + " | iconName=gedit bash=gedit terminal=false")
 print("---")
