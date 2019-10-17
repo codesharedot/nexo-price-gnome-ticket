1940cdccedec1a75d16b1fa336ae69e87f892116
codesharedot <codeto@sent.com>
2019-10-17 08:05:51 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# verge price gnome ticket 
+# crypto-com price gnome ticket 
 
-Gnome 3 desktop ticker for verge price with Python and argos. Rename to verge.1r.60s.py and put in ~/.config/argos
+Gnome 3 desktop ticker for crypto-com price with Python and argos. Rename to crypto-com.1r.60s.py and put in ~/.config/argos
 
 https://pythonbasics.org
--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# rename to verge.1r.60s.py and put in ~.config/argos
+# rename to crypto-com.1r.60s.py and put in ~.config/argos
 import re
 from gi.repository import Gio
 from datetime import datetime
@@ -9,7 +9,7 @@ import json
 import requests
 
 def coin():
-    data = requests.get("https://api.coinmarketcap.com/v1/ticker/verge/")
+    data = requests.get("https://api.coinmarketcap.com/v1/ticker/crypto-com/")
     price = data.json()[0]["price_usd"]
     coin_price = float(("{0:.2f}").format(float(price))) 
     return coin_price
@@ -18,7 +18,7 @@ usd = float(coin())
 lastupdate = datetime.now().strftime("%H:%M:%S")
 
 #print(" $" + str(usd) + " (" + str(lastupdate) + ") | iconName=invest-applet")
-print("verge $" + str(usd) + " | iconName=invest-applet")
+print("crypto-com $" + str(usd) + " | iconName=invest-applet")
 print("---")
 #print("Kraken: $" + str(usd) + " | iconName=gedit bash=gedit terminal=false")
 print("---")
